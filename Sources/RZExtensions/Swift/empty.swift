787e42f324341201d8b539f46f5fe61d44247193
Richard L Zarth III <rlziii@icloud.com>
2021-05-25 22:56:19 -0400

    Add empty functions.

---
+++ Sources/RZExtensions/Swift/empty.swift
@@ -0,0 +1,15 @@
+public func empty(_: A) {
+    // Do nothing.
+}
+
+public func empty(_: A, _: B) {
+    // Do nothing.
+}
+
+public func empty(_: A, _: B, _: C) {
+    // Do nothing.
+}
+
+public func empty(_: A, _: B, _: C, _: D) {
+    // Do nothing.
+}
