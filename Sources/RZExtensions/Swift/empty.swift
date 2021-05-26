9a4df44ee0eaf6e8c42d3c2ab2ddb3b71987e268
Richard L Zarth III <rlziii@icloud.com>
2021-05-25 23:02:02 -0400

    Add empty functions.

--- Sources/RZExtensions/Swift/empty.swift
+++ Sources/RZExtensions/Swift/empty.swift
@@ -1,15 +1,23 @@
+/// An empty function that does nothing and accepts one argument.
+/// Ideal to pass as a no-op in place of a closure instead of `{ _ in }` for example.
 public func empty(_: A) {
     // Do nothing.
 }
 
+/// An empty function that does nothing and accepts two arguments.
+/// Ideal to pass as a no-op in place of a closure instead of `{ _, _ in }` for example.
 public func empty(_: A, _: B) {
     // Do nothing.
 }
 
+/// An empty function that does nothing and accepts three arguments.
+/// Ideal to pass as a no-op in place of a closure instead of `{ _, _, _ in }` for example.
 public func empty(_: A, _: B, _: C) {
     // Do nothing.
 }
 
+/// An empty function that does nothing and accepts four arguments.
+/// Ideal to pass as a no-op in place of a closure instead of `{ _, _, _, _ in }` for example.
 public func empty(_: A, _: B, _: C, _: D) {
     // Do nothing.
 }
