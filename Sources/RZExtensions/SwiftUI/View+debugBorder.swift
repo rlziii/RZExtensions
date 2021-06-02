a9dd269f141f5e75cd32d561c453cd4b0232ff19
Richard L Zarth III <rlziii@icloud.com>
2021-06-01 22:38:03 -0400

    Add documentation to debugBorder.

--- Sources/RZExtensions/SwiftUI/View+debugBorder.swift
+++ Sources/RZExtensions/SwiftUI/View+debugBorder.swift
@@ -1,7 +1,11 @@
 import SwiftUI
 
 public extension View {
-    func debugBorder() -> some View {
-        border(Color.red)
+    /// Adds a colored border around views for debug purposes.
+    ///
+    /// - Parameter color: The color of the border. Defaults to red.
+    /// - Returns: A view that adds a border with the specified color to this view.
+    func debugBorder(color: Color = .red) -> some View {
+        border(color)
     }
 }
