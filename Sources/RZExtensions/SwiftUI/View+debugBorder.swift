9258b94715d9bcf4bfd24753ebd395079f93de1c
Richard L Zarth III <rlziii@icloud.com>
2021-06-01 22:40:39 -0400

    Add the option to customize the color and width of debugBorder(color:width:).

--- Sources/RZExtensions/SwiftUI/View+debugBorder.swift
+++ Sources/RZExtensions/SwiftUI/View+debugBorder.swift
@@ -3,9 +3,11 @@ import SwiftUI
 public extension View {
     /// Adds a colored border around views for debug purposes.
     ///
-    /// - Parameter color: The color of the border. Defaults to red.
-    /// - Returns: A view that adds a border with the specified color to this view.
-    func debugBorder(color: Color = .red) -> some View {
-        border(color)
+    /// - Parameters:
+    ///   - color: The color of the border. Defaults to red.
+    ///   - width: The width of the border. Defaults to 1.
+    /// - Returns: Adds a colored border around views for debug purposes.
+    func debugBorder(color: Color = .red, width: CGFloat = 1) -> some View {
+        border(color, width: width)
     }
 }
