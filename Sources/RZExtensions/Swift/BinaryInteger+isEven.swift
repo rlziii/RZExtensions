24e15251656efac20474e8fb42b5cd7c356cbcbf
Richard L Zarth III <rlziii@icloud.com>
2021-06-01 23:10:12 -0400

    Revert test.

--- Sources/RZExtensions/Swift/BinaryInteger+isEven.swift
+++ Sources/RZExtensions/Swift/BinaryInteger+isEven.swift
@@ -1,6 +1,6 @@
-extension BinaryInteger {
+public extension BinaryInteger {
     /// True if this value is an even number and false otherwise.
-    public var isEven: Bool {
+    var isEven: Bool {
         isMultiple(of: 2)
     }
 }
