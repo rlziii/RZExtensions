ad03cdcb2418b6b87767f75774f299294324a639
Richard L Zarth III <rlziii@icloud.com>
2021-06-01 23:08:47 -0400

    Testing...

--- Sources/RZExtensions/Swift/BinaryInteger+isEven.swift
+++ Sources/RZExtensions/Swift/BinaryInteger+isEven.swift
@@ -1,6 +1,6 @@
-public extension BinaryInteger {
+extension BinaryInteger {
     /// True if this value is an even number and false otherwise.
-    var isEven: Bool {
+    public var isEven: Bool {
         isMultiple(of: 2)
     }
 }
