70dff4089c807b9c0ce387e16d9d67c8adc2a360
Richard L Zarth III <rlziii@icloud.com>
2021-06-23 15:29:35 -0400

    Add void "instance."

---
+++ Sources/RZExtensions/Swift/void.swift
@@ -0,0 +1,6 @@
+/// A convenient "instance" of `Void` to make it more ergonomic to write instead of `()` where appropriate.
+/// - Example:
+///
+///       let subject = PassthroughSubject<Void, Never>()
+///       subject.send(void) // instead of `subject.send(())`
+public let void: Void = ()
