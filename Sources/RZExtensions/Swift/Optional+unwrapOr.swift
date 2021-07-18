b624cb2112531936e1bd8718b13af095b051747f
Richard L Zarth III <rlziii@icloud.com>
2021-07-18 01:41:53 -0400

    Add Optional.unwrap(or:file:line:).

---
+++ Sources/RZExtensions/Swift/Optional+unwrapOr.swift
@@ -0,0 +1,31 @@
+public extension Optional {
+    /// Attempts to unwrap an optional value and either returns the unwrapped value or, if `nil`, throws an assertion.
+    /// Assertions will generally stop the execution of the application in non-release builds (see: `assertionFailure(_:file:line:)`),
+    /// therefore this method takes a `fallback` parameter to return a safe fallback value for release builds.
+    ///
+    /// Example usage:
+    ///
+    ///     var nickname = user.nickname?.unwrap(or: "No nickname")
+    ///
+    /// In the above example, if the user has a nickname, then `nickname` would be assigned to that value. If the user
+    /// didn't have a nickname, then in a release build the value would be assigned to `"No nickname`" and in non-release
+    /// builds (e.g. debug builds) the application would crash with an `assertionFailure` and appropriate message.
+    ///
+    /// - Parameters:
+    ///   - fallback: The fallback value to use in release builds if the value is `nil`.
+    ///   - file: The file of the calling function; this should generally be left alone so the default value can be used.
+    ///   - line: The line of the calling function; this should generally be left along so the default value can be used.
+    /// - Returns: The unwrapped value if it exists; if `nil` then this returns the `fallback` value in release builds.
+    func unwrap(
+        or fallback: @autoclosure () -> Wrapped,
+        file: StaticString = #file,
+        line: UInt = #line
+    ) -> Wrapped {
+        guard let self = self else {
+            assertionFailure("Could not unwrap value of type [\(Wrapped.self)] in file [\(file)] at line [\(line)]. Returning value [\(function)] instead.")
+            return fallback()
+        }
+
+        return self
+    }
+}
