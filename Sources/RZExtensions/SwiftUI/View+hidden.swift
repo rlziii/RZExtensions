3251d0c74f814ea592a9c4d02b0c60bb1f39c5a0
Richard L Zarth III <rlziii@icloud.com>
2021-08-01 09:57:08 -0400

    Add documentation for View.hidden(_:).

--- Sources/RZExtensions/SwiftUI/View+hidden.swift
+++ Sources/RZExtensions/SwiftUI/View+hidden.swift
@@ -1,7 +1,8 @@
 import SwiftUI
 
+/// A `ViewModifier` that controls if a `View` is hidden or not conditionally.
 public struct HiddenViewModifier: ViewModifier {
-    public var isHidden: Bool
+    public let isHidden: Bool
 
     public func body(content: Content) -> some View {
         if isHidden {
@@ -13,6 +14,10 @@ public struct HiddenViewModifier: ViewModifier {
 }
 
 public extension View {
+    /// Hides this view conditionally.
+    ///
+    /// - Parameter isHidden: A boolean that determines if the view should be hidden (`true`) or not (`false`).
+    /// - Returns: A hidden or shown view.
     func hidden(_ isHidden: Bool) -> some View {
         modifier(HiddenViewModifier(isHidden: isHidden))
     }
