c6ec6ed66e6cc589e1dc662c869a8bd029228aa0
Richard L Zarth III <rlziii@icloud.com>
2021-08-01 17:37:52 -0700

    Comment clarification.

--- Sources/RZExtensions/SwiftUI/RepeatingView.swift
+++ Sources/RZExtensions/SwiftUI/RepeatingView.swift
@@ -1,12 +1,15 @@
 import SwiftUI
 
-/// A `View` that repeats its contents a specified number of times.
+/// A `View` that repeats its contents a specified **constant** number of times.
 ///
 /// For example, the following will repeat `SomeView()` three times:
 ///
 ///     RepeatingView(3) {
 ///       SomeView()
 ///     }
+///
+/// In practice this is very similar to using a `ForEach` with a range (i.e. `Range<Int>`)
+/// where the current iteration (e.g. `n`) is not necessary in the body of the `content`.
 public struct RepeatingView<Content: View>: View {
     private let count: Int
     @ViewBuilder private let content: Content
@@ -14,8 +17,8 @@ public struct RepeatingView<Content: View>: View {
     /// Creates a new `RepeatingView`.
     ///
     /// - Parameters:
-    ///   - count: A number of times the content should be repeated.
-    ///   - content: The contents that should be repeated.
+    ///   - count: The number of times the content should be repeated. **This should be a constant.**
+    ///   - content: The dynamic content that should be repeated.
     public init(_ count: Int, @ViewBuilder content: () -> Content) {
         self.count = count
         self.content = content()
