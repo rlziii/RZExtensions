26285aa20f904376b0d44dd04c5f25b3947aadad
Richard L Zarth III <rlziii@icloud.com>
2021-09-08 15:59:09 -0400

    Add GenericError.

---
+++ Sources/RZExtensions/Other/GenericError.swift
@@ -0,0 +1,17 @@
+/// A generic `Error` to be used when a custom `Error` type isn't needed.
+/// This should primarily be used for testing and other non-critical code paths.
+///
+/// Usage:
+///
+///     let error = GenericError("A nonrecoverable error occurred.")
+public struct GenericError: Error {
+    private let message: String
+
+    var localizedDescription: String { message }
+
+    /// Creates a new `GenericError` with an optional error message.
+    /// - Parameter message: An optional error message; defaults to an empty message.
+    init(_ message: String = "") {
+        self.message = message
+    }
+}
