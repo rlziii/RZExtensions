961e536a1ab30ca142e6f2746cb15cade5b29ac3
Richard L Zarth III <rlziii@icloud.com>
2021-09-08 15:41:26 -0400

    Add ObservableObject.objectDidChange publisher.

---
+++ Sources/RZExtensions/Combine/ObservableObject+objectDidChange.swift
@@ -0,0 +1,12 @@
+import Combine
+import Foundation
+
+public extension ObservableObject {
+    /// A publisher that emits **after** the object has changed.
+    /// Use this instead of `objectWillChange` to get events after an object has changed.
+    var objectDidChange: AnyPublisher<Self.ObjectWillChangePublisher.Output, Never> {
+        objectWillChange
+            .delay(for: 0.0, scheduler: RunLoop.current)
+            .eraseToAnyPublisher()
+    }
+}
