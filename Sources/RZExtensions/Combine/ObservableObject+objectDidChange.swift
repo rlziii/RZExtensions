67a3ed129ec372bf2e173345a536165c7eaf7456
Richard L Zarth III <rlziii@icloud.com>
2021-09-18 22:35:39 -0400

    Remove unneeded Self in generic definitions.

--- Sources/RZExtensions/Combine/ObservableObject+objectDidChange.swift
+++ Sources/RZExtensions/Combine/ObservableObject+objectDidChange.swift
@@ -4,7 +4,7 @@ import Foundation
 public extension ObservableObject {
     /// A publisher that emits **after** the object has changed.
     /// Use this instead of `objectWillChange` to get events after an object has changed.
-    var objectDidChange: AnyPublisher<Self.ObjectWillChangePublisher.Output, Never> {
+    var objectDidChange: AnyPublisher<ObjectWillChangePublisher.Output, Never> {
         objectWillChange
             .delay(for: 0.0, scheduler: RunLoop.current)
             .eraseToAnyPublisher()
--- Sources/RZExtensions/Combine/Publisher+mapToOptional.swift
+++ Sources/RZExtensions/Combine/Publisher+mapToOptional.swift
@@ -4,7 +4,7 @@ public extension Publisher {
     /// Creates a new publisher that transforms any upstream values wrapped into optional values.
     ///
     /// - Returns: A mapped stream of values wrapped as optionals.
-    func mapToOptional() -> Publishers.Map<Self, Self.Output?> {
+    func mapToOptional() -> Publishers.Map<Self, Output?> {
         map { Optional($0) }
     }
 }
